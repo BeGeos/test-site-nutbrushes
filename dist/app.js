2f1f987beceb76afafb0b6a6a1394003ccbc6d2c
BeGeos <montepeloso.m@gmail.com>
2021-12-19 14:28:42 +0100

    Major changes +  added wallpaper section

--- dist/app.js
+++ dist/app.js
@@ -1,46 +1,3 @@
-// Carousel
-const carouselSlide = document.querySelector(".sponsor__text");
-const carouselCards = document.querySelectorAll(".sponsor__container");
-
-// Buttons
-const prevBtn = document.querySelector("#prevBtn");
-const nextBtn = document.querySelector("#nextBtn");
-
-// Counter
-let counter = 1;
-let cardSize = 100;
-
-carouselSlide.style.transform = "translateX(" + -cardSize * counter + "%)";
-
-// Button listeners
-nextBtn.addEventListener("click", () => {
-  if (counter >= carouselCards.length - 1) return;
-  carouselSlide.style.transition = "transform 0.4s ease-in-out";
-  counter++;
-  carouselSlide.style.transform = "translateX(" + -cardSize * counter + "%)";
-});
-
-prevBtn.addEventListener("click", () => {
-  if (counter <= 0) return;
-  carouselSlide.style.transition = "transform 0.4s ease-in-out";
-  counter--;
-  carouselSlide.style.transform = "translateX(" + -cardSize * counter + "%)";
-});
-
-carouselSlide.addEventListener("transitionend", () => {
-  if (carouselCards[counter].id == "lastClone") {
-    carouselSlide.style.transition = "none";
-    counter = carouselCards.length - 2;
-    carouselSlide.style.transform = "translateX(" + -cardSize * counter + "%)";
-  }
-
-  if (carouselCards[counter].id == "firstClone") {
-    carouselSlide.style.transition = "none";
-    counter = carouselCards.length - counter;
-    carouselSlide.style.transform = "translateX(" + -cardSize * counter + "%)";
-  }
-});
-
 // Open Hamburger menu
 const hamburger = document.querySelector(".hamburger");
 const exitHamburger = document.querySelector(".exit-mobile-nav");
@@ -59,6 +16,24 @@ allLinks.forEach((link) => {
   link.addEventListener("click", () => {
     links.classList.remove("open");
   });
+});
+
+// Change language
+const englishBtn = document.getElementById("english");
+const italianBtn = document.getElementById("italian");
+
+englishBtn.addEventListener("click", (e) => {
+  e.target.classList.add("active");
+  italianBtn.classList.remove("active");
+  document.body.classList.add("en");
+  document.body.classList.remove("it");
+});
+
+italianBtn.addEventListener("click", (e) => {
+  e.target.classList.add("active");
+  englishBtn.classList.remove("active");
+  document.body.classList.add("it");
+  document.body.classList.remove("en");
 });
 
 // Modal for single brushes
@@ -111,249 +86,65 @@ openModalBtns.forEach((button) => {
   });
 });
 
-// Sponsor section open/close bio-highlights description
-// Navigation buttons
-
-// Laura Rogora Sponsor
-const lauraNavigationBtns = document.querySelectorAll("#laura-rogora button");
-lauraNavigationBtns.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    let currentSponsor = btn.dataset.modalTarget;
-    let shownText = document.querySelector("#laura-rogora .show");
-    let divToOpen = document.getElementById(currentSponsor);
-    const activeBtn = document.querySelector("#laura-rogora button.active");
-    if (activeBtn.id !== btn.id) {
-      activeBtn.classList.remove("active");
-      btn.classList.add("active");
-    } else {
-      return;
-    }
-
-    if (divToOpen.id !== shownText.id) {
-      shownText.classList.remove("show");
-      divToOpen.classList.add("show");
-    } else {
-      return;
-    }
-  });
-});
-
 // Navigation mid section
 const navBtns = document.querySelectorAll(".mid-section .nav-text");
+const firstMidTextEng = document.getElementById("first");
+const firstMidTextIt = document.getElementById("first-it");
+const secondMidTextEng = document.getElementById("second");
+const secondMidTextIt = document.getElementById("second-it");
+const thirdMidTextEng = document.getElementById("third");
+const thirdMidTextIt = document.getElementById("third-it");
+
+function makeInvisible() {
+  let arr = [
+    firstMidTextIt,
+    firstMidTextEng,
+    secondMidTextIt,
+    secondMidTextEng,
+    thirdMidTextIt,
+    thirdMidTextEng,
+  ];
+  return arr.forEach((item) => item.classList.remove("visible"));
+}
+
+function makeVisible(item) {
+  item.classList.add("visible");
+}
+
+function activateBtn(target) {
+  navBtns.forEach((btn) => {
+    if (btn == target) return;
+    btn.classList.remove("active");
+    target.classList.add("active");
+  });
+}
 
 navBtns.forEach((btn) => {
   btn.addEventListener("click", (e) => {
-    // Give active status to button
-    let activeNavBtn = document.querySelector(".mid-section .nav-text.active");
-    activeNavBtn.classList.remove("active");
-    btn.classList.add("active");
-
-    // Give visible class to text
-    let visibleText = document.querySelector(
-      ".mid-section .infra__text .visible"
-    );
-    visibleText.classList.remove("visible");
-    let _id = e.target.getAttribute("dataset");
-    let targetText = document.getElementById(_id);
-    targetText.classList.add("visible");
+    let currentLang = document.body.getAttribute("class");
+    let dataset = e.target.getAttribute("dataset");
+    let visible;
+    switch (dataset) {
+      case "first":
+        activateBtn(e.target);
+        makeInvisible();
+        visible = currentLang == "it" ? firstMidTextIt : firstMidTextEng;
+        return makeVisible(visible);
+      case "second":
+        activateBtn(e.target);
+        makeInvisible();
+        visible = currentLang == "it" ? secondMidTextIt : secondMidTextEng;
+        return makeVisible(visible);
+      case "third":
+        activateBtn(e.target);
+        makeInvisible();
+        visible = currentLang == "it" ? thirdMidTextIt : thirdMidTextEng;
+        return makeVisible(visible);
+    }
   });
 });
 
-// closeModalBtns.forEach((button) => {
-//   button.addEventListener("click", () => {
-//     const modal = button.closest(".modal");
-//     closeModal(modal);
-//   });
-// });
-
-// overlay.addEventListener("click", () => {
-//   const modals = document.querySelectorAll(".modal.active");
-//   modals.forEach((modal) => {
-//     closeModal(modal);
-//   });
-// });
-
-// // Open modal
-// function openModal(modal) {
-//   if (modal == null) return;
-//   overlay.classList.add("active");
-//   modal.classList.add("active");
-// }
-
-// // Close modal
-// function closeModal(modal) {
-//   if (modal == null) return;
-//   modal.classList.remove("active");
-//   overlay.classList.remove("active");
-// }
-
-// Modal for wood types
-// Modal opening and closing
-// const openModalBtn = document.getElementById("woods__btn");
-// const woodsModal = document.getElementById("woods");
-// const closeModalBtn = document.querySelector("[data-close-button]");
-// const overlay = document.querySelector(".overlay");
-
-// openModalBtn.addEventListener("click", () => {
-//   openModal(woodsModal);
-// });
-
-// closeModalBtn.addEventListener("click", (e) => {
-//   const modal = e.target.closest(".modal");
-//   closeModal(modal);
-// });
-
-// overlay.addEventListener("click", () => {
-//   const modals = document.querySelectorAll(".modal.active");
-//   modals.forEach((modal) => {
-//     closeModal(modal);
-//   });
-// });
-
-// // Open modal
-// function openModal(modal) {
-//   if (modal == null) return;
-//   overlay.classList.add("active");
-//   modal.classList.add("active");
-// }
-
-// // Close modal
-// function closeModal(modal) {
-//   if (modal == null) return;
-//   modal.classList.remove("active");
-//   overlay.classList.remove("active");
-// }
-
-// // Change selection of woods btns
-// const modalTitle = document.querySelector(".modal__title h2");
-// const modalImage = document.getElementById("wood__img");
-// const modalText = document.querySelector(".modal__text");
-
-// // Pic animation on hover
-// modalImage.addEventListener("mousemove", (e) => {
-//   modalImage.style.backgroundPositionX =
-//     "calc(125px + " + -e.offsetX * 0.5 + "px)";
-//   modalImage.style.backgroundPositionY =
-//     "calc(50px + " + -e.offsetY * 0.5 + "px)";
-// });
-
-// modalImage.addEventListener("mouseleave", (e) => {
-//   modalImage.style.backgroundPositionX = "0px";
-//   modalImage.style.backgroundPositionY = "0px";
-// });
-
-// // palissandro
-// const palissandroBtn = document.getElementById("palissandro");
-// palissandroBtn.addEventListener("click", (e) => {
-//   let activeBtns = document.querySelectorAll(".modal__options .active");
-//   activeBtns.forEach((button) => button.classList.remove("active"));
-//   palissandroBtn.classList.add("active");
-//   modalTitle.textContent = "Palissandro Santos";
-//   modalText.innerHTML = "<p>Palissandro is a very light wood bla bla bla</p>";
-//   modalImage.style.backgroundImage =
-//     "url(./images/wood_types/palissandro.jpeg)";
-// });
-
-// // ebano
-// const ebanoBtn = document.getElementById("ebano");
-// ebanoBtn.addEventListener("click", (e) => {
-//   let activeBtns = document.querySelectorAll(".modal__options .active");
-//   activeBtns.forEach((button) => button.classList.remove("active"));
-//   ebanoBtn.classList.add("active");
-//   modalTitle.textContent = "Ebano Africano";
-//   modalText.innerHTML = "<p>Ebony is a very dark wood bla bla bla</p>";
-//   modalImage.style.backgroundImage = "url(./images/wood_types/ebony.jpeg)";
-// });
-
-// // Padouk
-// const padoukBtn = document.getElementById("padouk");
-// padoukBtn.addEventListener("click", (e) => {
-//   let activeBtns = document.querySelectorAll(".modal__options .active");
-//   activeBtns.forEach((button) => button.classList.remove("active"));
-//   padoukBtn.classList.add("active");
-//   modalTitle.textContent = "Padouk";
-//   modalText.innerHTML = "<p>Padouk is a very amber wood bla bla bla</p>";
-//   modalImage.style.backgroundImage = "url(./images/wood_types/padouk.jpeg)";
-// });
-
-// // Pao Rosa
-// const paoBtn = document.getElementById("pao");
-// paoBtn.addEventListener("click", (e) => {
-//   let activeBtns = document.querySelectorAll(".modal__options .active");
-//   activeBtns.forEach((button) => button.classList.remove("active"));
-//   paoBtn.classList.add("active");
-//   modalTitle.textContent = "Pao Rosa";
-//   modalText.innerHTML = "<p>Pao Rosa is a very pinky wood bla bla bla</p>";
-//   modalImage.style.backgroundImage = "url(./images/wood_types/pao-rosa.jpeg)";
-// });
-
-// // Amaranto
-// const amarantoBtn = document.getElementById("amaranto");
-// amarantoBtn.addEventListener("click", (e) => {
-//   let activeBtns = document.querySelectorAll(".modal__options .active");
-//   activeBtns.forEach((button) => button.classList.remove("active"));
-//   amarantoBtn.classList.add("active");
-//   modalTitle.textContent = "Amaranto";
-//   modalText.innerHTML = "<p>Amaranto is a red wood bla bla bla</p>";
-//   modalImage.style.backgroundImage = "url(./images/wood_types/amaranto.jpeg)";
-// });
-
-// // Zebrano
-// const zebranoBtn = document.getElementById("zebrano");
-// zebranoBtn.addEventListener("click", (e) => {
-//   let activeBtns = document.querySelectorAll(".modal__options .active");
-//   activeBtns.forEach((button) => button.classList.remove("active"));
-//   zebranoBtn.classList.add("active");
-//   modalTitle.textContent = "Zebrano";
-//   modalText.innerHTML = "<p>Zebrano is a red wood bla bla bla</p>";
-//   modalImage.style.backgroundImage = "url(./images/wood_types/zebrano.jpeg)";
-// });
-
-// // Wengè
-// const wengeBtn = document.getElementById("wenge");
-// wengeBtn.addEventListener("click", (e) => {
-//   let activeBtns = document.querySelectorAll(".modal__options .active");
-//   activeBtns.forEach((button) => button.classList.remove("active"));
-//   wengeBtn.classList.add("active");
-//   modalTitle.textContent = "Wengè";
-//   modalText.innerHTML = "<p>Wengè is a red wood bla bla bla</p>";
-//   modalImage.style.backgroundImage = "url(./images/wood_types/wenge.jpeg)";
-// });
-
-// // Ziricote
-// const ziricoteBtn = document.getElementById("ziricote");
-// ziricoteBtn.addEventListener("click", (e) => {
-//   let activeBtns = document.querySelectorAll(".modal__options .active");
-//   activeBtns.forEach((button) => button.classList.remove("active"));
-//   ziricoteBtn.classList.add("active");
-//   modalTitle.textContent = "Ziricote";
-//   modalText.innerHTML = "<p>Ziricote is a red wood bla bla bla</p>";
-//   modalImage.style.backgroundImage = "url(./images/wood_types/ziricote.jpeg)";
-// });
-
-// // Palma
-// const palmaBtn = document.getElementById("palma");
-// palmaBtn.addEventListener("click", (e) => {
-//   let activeBtns = document.querySelectorAll(".modal__options .active");
-//   activeBtns.forEach((button) => button.classList.remove("active"));
-//   palmaBtn.classList.add("active");
-//   modalTitle.textContent = "Palma";
-//   modalText.innerHTML = "<p>Palma is a red wood bla bla bla</p>";
-//   modalImage.style.backgroundImage = "url(./images/wood_types/palma.jpeg)";
-// });
-
-// // Ulivo
-// const ulivoBtn = document.getElementById("ulivo");
-// ulivoBtn.addEventListener("click", (e) => {
-//   let activeBtns = document.querySelectorAll(".modal__options .active");
-//   activeBtns.forEach((button) => button.classList.remove("active"));
-//   ulivoBtn.classList.add("active");
-//   modalTitle.textContent = "Ulivo";
-//   modalText.innerHTML = "<p>Ulivo is a red wood bla bla bla</p>";
-//   modalImage.style.backgroundImage = "url(./images/wood_types/ulivo.jpeg)";
-// });
-
-// LAzy loading with Intersection Observer
+// Lazy loading with Intersection Observer
 let options = {
   root: null,
   rootMargin: "80px",
@@ -377,3 +168,11 @@ document.querySelectorAll("[data-load]").forEach((section) => {
     observer.observe(section);
   }
 });
+
+// masonry for wallpaper
+// var elem = document.querySelector(".poster__grid");
+// var msnry = new Masonry(elem, {
+//   // options
+//   itemSelector: ".poster",
+//   columnWidth: 250,
+// });
